821623a8920d7a73762e68738a21236bcdd762a4
Brandon Ferrell <brandon.bryanferrell@gmail.com>
2016-04-10 16:49:04 -0500

    Adding drawables

---
+++ UTDallas/app/src/main/java/com/example/shounakk/utdallas/pages/GroupsFragment.java
@@ -0,0 +1,122 @@
+package com.example.shounakk.utdallas.pages;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.shounakk.utdallas.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link GroupsFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link GroupsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class GroupsFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    ViewPager mPager;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment GroupsFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static GroupsFragment newInstance(String param1, String param2) {
+        GroupsFragment fragment = new GroupsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public GroupsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_groups, container, false);
+
+        FeaturedPagerAdapter adapter = new FeaturedPagerAdapter();
+        mPager = (ViewPager) v.findViewById(R.id.featuredpager);
+        mPager.setAdapter(adapter);
+
+        mPager.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                v.getParent().requestDisallowInterceptTouchEvent(true);
+                return false;
+            }
+        });
+
+        mPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                mPager.getParent().requestDisallowInterceptTouchEvent(true);
+            }
+        });
+
+        return v;
+    }
+
+    class FeaturedPagerAdapter extends PagerAdapter {
+
+        public Object instantiateItem(ViewGroup collection, int position) {
+
+            int resId = 0;
+            switch (position) {
+                case 0:
+                    resId = R.id.banner1;
+                    break;
+                case 1:
+                    resId = R.id.banner2;
+                    break;
+            }
+            return getActivity().findViewById(resId);
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Override
+        public boolean isViewFromObject(View arg0, Object arg1) {
+            return arg0 == ((View) arg1);
+        }
+    }
+
+
+}
