e096fc893e239cae07ce19120c42a3e296625b8d
Shounak Kulkarni <shounakkul@Gmail.com>
2016-06-14 01:15:37 -0500

    Added Chrome Custom Tabs for FB login

--- UTDallas/app/src/main/java/com/example/shounakk/utdallas/SignInActivity.java
+++ UTDallas/app/src/main/java/com/example/shounakk/utdallas/SignInActivity.java
@@ -2,10 +2,42 @@ package com.example.shounakk.utdallas;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class SignInActivity extends AppCompatActivity{
+    CallbackManager callbackManager;
+    LoginButton loginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Toast.makeText(getApplicationContext(), "Facebook login onSuccess", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(getApplicationContext(), "Facebook login onCancel", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                Toast.makeText(getApplicationContext(), "Facebook login onError", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
